c735b64e590e7dd4d223644509b7087db5cf4209
tmricardo7 <tmricardo7@gmail.com>
2015-10-29 18:48:19 -0600

    until step 6

---
+++ simple-todos.js
@@ -0,0 +1,40 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+
+    Template.body.helpers({
+        tasks: function(){
+            return Tasks.find({}, {sort: {createdAt: -1}});
+        }
+    });
+
+    Template.body.events({
+        "submit .new-task": function() {
+            event.preventDefault();
+            var text = event.target.text.value;
+            Tasks.insert({
+                text: text,
+                createdAt: new Date(),
+                checked: false
+            });
+
+            event.target.text.value = "";
+        }
+    });
+
+    Template.task.events({
+        "click .toggle-checked": function(){
+            Tasks.update(this._id, {
+               $set: {checked: ! this.checked}
+            });
+        },
+        "click .delete": function(){
+            Tasks.remove(this._id);
+        }
+    });
+}
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
+}
